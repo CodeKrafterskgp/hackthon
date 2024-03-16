b9a2ef567820b589b3ab64feda97e733f5c184c7
Adarshchaudhari300 <krsadarsh@gmail.com>
2024-03-16 20:06:59 +0530

    navbar footer landing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import Leader from './components/leader-board/Leader';
-import Login from './components/Login/Login';
+// import Leader from './components/leader-board/Leader';
+// import Login from './components/Login/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/navbar/navbar";
+import Footer from './components/footer/footer';
+import Landing from './components/Landing/landing';
+
 
 const App = () => {
-  return (
-    <div>
-      {/* <Leader /> */}
-      <Login />
+  return (<Router>
+    <div className="App">
+      <div className="bg2">
+
+        <Navbar />
+
+
+        <Switch>
+
+
+          <Route exact path="/">
+            <Landing />
+          </Route>
+
+
+
+        </Switch>
+
+
+        <Footer />
+      </div>
     </div>
+  </Router>
   );
 };
 
---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css';
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="bl1">
+                <p>9012160141</p>
+                <p>krsadarsh@gmail.com</p>
+            </div>
+            <div className="bl1">
+                <p>ABout us : Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet voluptas ut labore ullam quasi at quia soluta temporibus modi illo!</p>
+            </div>
+            <div className="bl2">
+                <p>Copyright</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
